86cd359ca0b3da5a31d3893ab7bfc674bad2bd70
zbates1 <zanetbates1@gmail.com>
2024-08-27 17:18:38 -0400

    admin + home changes

--- website/__init__.py
+++ website/__init__.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
 from .config import Config # this is to setup the config.py for env variables
 
 db = SQLAlchemy()
@@ -33,6 +36,10 @@ def create_app():
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
+
+    # Admin setup
+    admin = Admin(app)
+    admin.add_view(ModelView(User, db.session))
 
     @login_manager.user_loader
     def load_user(id):
